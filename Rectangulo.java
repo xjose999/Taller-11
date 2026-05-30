563769e4249ddfc42df50c6e6ca51a740be40a82
xjose999 <josejoestar80@gmail.com>
2026-05-30 01:58:25 -0500

    Ejercicio 1.2

---
+++ Rectangulo.java
@@ -0,0 +1,9 @@
+// 2. Clase derivada Rectangulo
+class Rectangulo extends Figura {
+    double base;
+    double altura;
+
+    public double calcularArea() {
+        return base * altura;
+    }
+}
